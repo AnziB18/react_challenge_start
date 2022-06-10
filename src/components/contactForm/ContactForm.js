74a3068c2eb79981570ef9b21b6ddd5a22546ca0
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-10 16:33:34 +0200

    added props &  inputs

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 /*  name,
   setName,
   phone,
@@ -8,5 +8,38 @@ import React, {useState} from "react";
   handleSubmit  return (
     ContactForm
   );*/
-export const ContactForm = (props) => {
-}
+
+  export const ContactForm = (props) => {
+    return (
+      <div>
+        <form onSubmit={props.onSubmit}>
+          <input  type='text' 
+                  placeholder="Enter new name" 
+                  value={props.addFirstname} 
+                  onChange={({target}) => props.setFirstname(target.value)}
+                  name='Firstname'
+                  //id="name"
+                  ></input>
+          <input  type='tel' 
+                  placeholder="Enter new phone number" 
+                  value={props.addPhone} 
+                  onChange={({target}) => props.setPhone(target.value)}
+                  name='Phone'
+                  //id="phone"
+                  ></input>
+          <input  type='mail' 
+                  placeholder="Enter new Email" 
+                  value={props.addEmail}
+                  onChange={({target}) => props.setEmail(target.value)}
+                  name='mail'
+                  //id="mail"
+                  ></input>
+          <input type="submit" value="Submit" />
+          {/* <button >Submit</button> */}
+        </form>
+        {/* <p>{`${props.addContactInfo[0][0]}`}</p> */}
+      </div>
+    )
+  };
+  
+  
