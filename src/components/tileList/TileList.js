bb8d4ee54030683585139e6f8bbbdb51ef35a485
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-11 11:24:35 +0200

    changed mapping for array to pass it to Tile

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -4,11 +4,11 @@ import { Tile } from "../tile/Tile";
 export const TileList = (props) => {
   return (
     <div>
-           {props.info.map((element, index) => (
+           {props.info.map((object, index) => (
                 <Tile key={`tileKey: ${index}`}
-                      // titlename={(Object.keys(element))} 
-                      // titlevalue={Object.values(element)}
-                      titleValue={element}
+                       //titlename={(Object.keys(object))} 
+                       //titlevalue={Object.values(object)}
+                        titleValue={object}
                 />
               ))}
     </div>
