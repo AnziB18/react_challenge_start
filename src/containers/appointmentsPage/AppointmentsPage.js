d1b4cf5470a8cd2716c31ef254df7f9bbbf76951
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-11 13:02:26 +0200

    added variables, useState & handleSubmit

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React,{ useState }  from "react";
+import {AppointmentForm} from "../../components/appointmentForm/AppointmentForm.js"
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = (props) => {
+  /* Define state variables for appointment info */
+  const [currentTitle, setCurrentTitle] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
+  const contactInfoName = props.allContactInfo;
+  const appointments = props.appointments;
+  const addAppointments = props.onAppointmentAdd;
+  const resetInput = () => {
+    setCurrentTitle('');
+    setContact('');
+    setDate('');
+    setTime('');
+}
   const handleSubmit = (e) => {
-    e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+    e.preventDefault();/* Add contact info and clear data  */
+    addAppointments([{Title: currentTitle}, {ContactPerson: contact}, {Date: date}, {Time: time}]);
+    resetInput();
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm  addCurrentTitle={currentTitle}
+                          addContact={contact}
+                          addDate={date}
+                          addTime={time}
+                          addsetCurrentTitle={setCurrentTitle}
+                          addsetContact={setContact}
+                          addsetDate={setDate}
+                          addsetTime={setTime}
+                          onhandleSubmit={handleSubmit}
+                          />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList info={appointments}     
+                  />
       </section>
     </div>
   );
