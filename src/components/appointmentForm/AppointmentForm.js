4870098e5c95eb7d2d41190b0d11475214c96cb9
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-11 13:20:32 +0200

    added main content for render appointments

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
-export const AppointmentForm = ({
-  contacts,
+/*    contacts,
   title,
   setTitle,
   contact,
@@ -10,8 +10,9 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
-}) => {
+  handleSubmit */
+
+export const AppointmentForm = (props) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
       .toLocaleDateString("en-US")
@@ -20,6 +21,39 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <div>
+      <form onSubmit={props.onSubmit}>
+        <input  type='text' 
+                placeholder="Enter new Appointment Title" 
+                value={props.addFirstname} 
+                onChange={({target}) => props.addsetCurrentTitle(target.value)}
+                name='title'
+                //id="name"
+                />
+        <input  type='date' 
+                placeholder="Enter new Date" 
+                value={props.addPhone} 
+                onChange={({target}) => props.addsetDate(target.value)}
+                name='date'
+                //id="phone"
+                />
+        <input  type='time' 
+                placeholder="Enter new Time" 
+                value={props.addEmail}
+                onChange={({target}) => props.addsetTime(target.value)}
+                name='time'
+                //id="mail"
+                />
+        <input  type='text' 
+                placeholder="Pick a Contact" 
+                //value={props.addEmail}
+                //onChange={({target}) => props.addsetTime(target.value)}
+                //name='time'
+                //id="mail"
+                />
+        <input type="submit" value="Submit" />
+        {/* <button >Submit</button> */}
+      </form>
+    </div>
   );
 };
