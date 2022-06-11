852c20e7bc2aa2dc1007182c042f35c38023611a
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-11 07:24:02 +0200

    add content to render tiles

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
   return (
     <div className="tile-container">
-      
+        {Object.values(props.titleValue).map((element,index) => 
+          <>
+            <p className="tile-title" key={`Objkey: ${index}`}>{element[0]}</p>
+            <p className="tile" key={`Objkey: ${index}`}>{element}</p>
+          </>
+        )
+        }
     </div>
   );
 };
