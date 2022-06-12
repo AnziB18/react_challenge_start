11f9daded7d766d9a53f9666703676a276ca948e
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-12 08:24:32 +0200

    map through array pass props to tile

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,14 @@
 import React from "react";
-
-export const TileList = () => {
+import { Tile } from "../tile/Tile"; 
+export const TileList = (props) => {
   return (
     <div>
-      
+           {props.info.map((object, key) => (
+                <Tile key={`tileKey: ${key}`}
+                        titleValue={object}
+                />
+              ))}
     </div>
+
   );
 };
