3f07cb70d3eb6953ff7b09467730b5f19bc48f73
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-12 08:26:12 +0200

    add return content to render tiles

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
+  let newArr = props.titleValue;
+  let firstSlice = newArr.slice(0,1);
+  let restSlice = newArr.slice(1)
+
   return (
     <div className="tile-container">
-      
+      <p className="tile-title" key={`${Object.values(firstSlice[0])}`}>{Object.keys(firstSlice[0])}: {Object.values(firstSlice[0])}</p>
+      {restSlice.map((element) => 
+      <p className="tile" key={`${Object.values(element)}`}>{`${Object.keys(element)}: ${Object.values(element)}`}</p>)}
     </div>
   );
 };
