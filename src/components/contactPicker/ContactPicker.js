b0b2ec06a15872678b458154ca2b082edd11d921
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-12 08:39:33 +0200

    added main content to ContactPicker component

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
+  const currentContacts = Array.from(props.arrayOfContacts);
+  let flatArray = currentContacts.flat();
+   let lastArray = [];
+   let filterArray=flatArray.filter(({Firstname}) => {
+          return Firstname
+      }) 
+    for(let i=0; i<=filterArray.length-1; i++){
+      console.log(filterArray[i].Firstname);
+      lastArray.push((filterArray[i].Firstname));
+      console.log(lastArray)
+    }
+  const listItem = lastArray.map((item, key) => <option value={props.currentContact} key={key}>{item}</option> )
   return (
-    ContactPicker
+       <select onChange={props.onChange}>
+            <option defaultValue="" selected='selected'>--Please choose an option--</option>
+            {listItem}
+      </select>
   );
 };
