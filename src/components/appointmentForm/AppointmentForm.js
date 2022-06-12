14568ade1bdf3e9985847b14453fd89f298bce67
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-12 08:35:31 +0200

    added inputs & handle events & props to component

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,17 +1,7 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
-export const AppointmentForm = ({
-  contacts,
-  title,
-  setTitle,
-  contact,
-  setContact,
-  date,
-  setDate,
-  time,
-  setTime,
-  handleSubmit
-}) => {
+export const AppointmentForm = (props) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
       .toLocaleDateString("en-US")
@@ -20,6 +10,33 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <div>
+    <form onSubmit={props.onSubmit}>
+      <input  type='text' 
+              placeholder="Enter new Appointment Title" 
+              value={props.addCurrentTitle} 
+              onChange={({target}) => props.addsetCurrentTitle(target.value)}
+              name='title'
+              />
+      <input  type='date' 
+              placeholder="Enter new Date" 
+              value={props.addDate} 
+              onChange={({target}) => props.addsetDate(target.value)}
+              name='date'
+              min={"2022-01-01"}
+              />
+      <input  type='time' 
+              placeholder="Enter new Time" 
+              value={props.addTime}
+              onChange={({target}) => props.addsetTime(target.value)}
+              name='time'
+              />
+      <ContactPicker  arrayOfContacts={props.arrayOfContacts}
+                      onChange={({target}) => props.addsetContact(target.value)}
+                      />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+
   );
 };
