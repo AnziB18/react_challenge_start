8d759e16bb5abd9e1eb1841ee7d73043e937112b
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-12 08:21:31 +0200

    added input to render fields for adding contacts

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,12 +1,30 @@
-import React, {useState} from "react";
-/*  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit  return (
-    ContactForm
-  );*/
+import React from "react";
+
 export const ContactForm = (props) => {
+  return (
+    <div>
+    <form onSubmit={props.onSubmit}>
+      <input  type='text' 
+              placeholder="Enter new name" 
+              value={props.addFirstname} 
+              onChange={({target}) => props.setFirstname(target.value)}
+              name='Firstname'
+              />
+      <input  type='tel' 
+              placeholder="Enter new phone number" 
+              value={props.addPhone} 
+              onChange={({target}) => props.setPhone(target.value)}
+              name='Phone'
+              />
+      <input  type='mail' 
+              placeholder="Enter new Email" 
+              value={props.addEmail}
+              onChange={({target}) => props.setEmail(target.value)}
+              name='mail'
+              />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+
+  )
 }
