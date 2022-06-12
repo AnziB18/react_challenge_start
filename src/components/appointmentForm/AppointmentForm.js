73efc68b63b41b1f161bc5b4b22a2c132fb80445
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-12 07:40:02 +0200

    add ContactPicker componet and pass props

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -13,47 +13,63 @@ import { ContactPicker } from "../contactPicker/ContactPicker";
   handleSubmit */
 
 export const AppointmentForm = (props) => {
-  const getTodayString = () => {
-    const [month, day, year] = new Date()
-      .toLocaleDateString("en-US")
-      .split("/");
-    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
-  };
+  // const getTodayString = () => {
+  //   const [month, day, year] = new Date()
+  //     .toLocaleDateString("en-US")
+  //     .split("/");
+  //   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  // };
 
   return (
     <div>
       <form onSubmit={props.onSubmit}>
         <input  type='text' 
                 placeholder="Enter new Appointment Title" 
-                value={props.addFirstname} 
+                value={props.addCurrentTitle} 
                 onChange={({target}) => props.addsetCurrentTitle(target.value)}
                 name='title'
                 //id="name"
                 />
         <input  type='date' 
                 placeholder="Enter new Date" 
-                value={props.addPhone} 
+                value={props.addDate} 
                 onChange={({target}) => props.addsetDate(target.value)}
                 name='date'
+                min={"2022-01-01"}
                 //id="phone"
                 />
         <input  type='time' 
                 placeholder="Enter new Time" 
-                value={props.addEmail}
+                value={props.addTime}
                 onChange={({target}) => props.addsetTime(target.value)}
                 name='time'
                 //id="mail"
                 />
-        <input  type='text' 
+        <ContactPicker  //currentContact={props.addContact}
+                        arrayOfContacts={props.arrayOfContacts}
+                        onChange={({target}) => props.addsetContact(target.value)}
+                        />
+          {/* { props.addContact.map((object, key) => (
+          <ContactPicker  currentContact={object}
+                          key={key}
+                          onChange={({target}) => props.addsetContact(target.value)}
+                        />
+        ) 
+        )}  */}
+        {/* <input  type='text' 
                 placeholder="Pick a Contact" 
-                //value={props.addEmail}
-                //onChange={({target}) => props.addsetTime(target.value)}
+                value={props.addContact}
+                onChange={({target}) => props.addsetContact(target.value)}
                 //name='time'
                 //id="mail"
-                />
+                /> */}
+                
         <input type="submit" value="Submit" />
         {/* <button >Submit</button> */}
       </form>
+    <div>
+    {/* <ContactPicker arrayOfContacts={props.arrayOfContacts}/> this is not visible for ContactPicker.js!! */}
+    </div>
     </div>
   );
 };
